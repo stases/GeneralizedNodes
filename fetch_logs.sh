808abf5e726677a28e30b18694010bdc8d8bbe9e
stases <tin@fizika.hr>
2023-04-26 17:01:40 +0200

    Added ssh log remote fetcher

---
+++ fetch_logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the source and destination paths
+source_path="thadzi@snellius.surf.nl:/home/thadzi/GitHub/FractalMessagePassing/logs"
+dest_path="/home/tin/Documents/Github/FractalMessagePassing/downloaded_logs/"
+
+# Copy the logs folder from the remote server to the local machine
+echo "Copying logs folder from remote server..."
+scp -r "$source_path" "$dest_path"
+
+# Start TensorBoard on the local machine
+echo "Starting TensorBoard on local machine..."
+tensorboard --logdir="$dest_path"
