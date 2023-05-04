3215f45a011a09e262f7433177f5eb0b7b38d84b
stases <tin@fizika.hr>
2023-05-04 18:12:24 +0200

    Fixing batching

--- trainers/get_fw_function.py
+++ trainers/get_fw_function.py
@@ -1,7 +1,6 @@
 def get_forward_function(model, data, Z_ONE_HOT_DIM, **kwargs):
     model_name = model.name
     if model_name in ['TransformerNet', 'FractalNet', 'FractalNetShared']:
-        data.batch = data.batch[data.ground_node]
         out = model(data.x[:, :Z_ONE_HOT_DIM], data.edge_index, data.subgraph_edge_index,
               data.node_subnode_index, data.subnode_node_index,
               data.ground_node, data.subgraph_batch_index, data.batch, **kwargs)
